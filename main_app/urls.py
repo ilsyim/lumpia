674ab93e04743eac2ea41f8f7be314e603fa9936
ilysim <illymarie1@live.com>
2022-08-09 10:31:54 -0500

    add some styling & home/index views

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
   path('', views.home, name='home'),
   path('about/', views.about, name='about'),
+  path('lumpias/', views.lumpias_index, name='lumpias_index'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,10 +1,24 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
+
+class Lumpia:
+  def __init__(self, protein, fillings, review):
+    self.protein = protein
+    self.filings = fillings 
+    self.review = review
+  
+lumpias = [
+  Lumpia('fish', 'noodles and veggies', 'So fresh and so clean'),
+  Lumpia('pork', 'glass noodles and veggies', 'Crunchy, best when fried'),
+  Lumpia('tofu', 'onions, carrots, soy sauce', 'No meat!')
+]
 # Create your views here.
 
 def home(request):
-  return HttpResponse('<h1>Lumpia</h1>')
+  return render(request, 'home.html')
 
 def about(request):
-  return HttpResponse('<h1>This is the about page</h1>')
+  return render(request, 'about.html')
+
+def lumpias_index(request):
+  return render(request, 'lumpias/index.html', {'lumpias': lumpias})
